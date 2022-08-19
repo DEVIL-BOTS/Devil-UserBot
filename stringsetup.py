11216fcc7de279d94cf0867b84674b4d68e36888
DevilBotz <110766400+DEVIL-BOTS@users.noreply.github.com>
2022-08-19 22:23:51 +0530

    Update stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -2,7 +2,7 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 print("")
-print("""Welcome To W2HBot String Session\nGenerator By @W2HOwner\n\n""")
+print("""Welcome To Devil-UserBot String Session\nGenerator By @ARG_GAMING_9300\n\n""")
 print("""Enter Your Valid Details To Continue!\n\n """)
 
 API_KEY = input("API_ID:  ")
@@ -18,11 +18,11 @@ while True:
             session = client.session.save()
             client.send_message(
                 "me",
-                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it) \n\n `{session}` \n\n And Visit @W2HSupport For Any Help!\n\n",
+                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it) \n\n `{session}` \n\n And Visit @DevilBotz_Support For Any Help!\n\n",
             )
 
             print(
-                "Thanks for Choosing W2HBot Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
+                "Thanks for Choosing Devil-UserBot Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
             )
     except:
         print("")
